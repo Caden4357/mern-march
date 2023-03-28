17b7d7912af55b08d7e22bc134065fa0f4986dba
Caden Wilcox <wilcox4357@gmail.com>
2023-03-28 16:02:57 -0700

    algos

---
+++ week4/day1/react-router-dom/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Routing in React with react-router-dom! Dont forget to npm react-router-dom to get started</h1>
+    </div>
+  );
+}
+
+export default App;
