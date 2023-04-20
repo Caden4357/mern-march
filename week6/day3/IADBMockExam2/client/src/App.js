ad0c7b78af7d7c598916f2b5695ab0c05fdfd10a
Caden Wilcox <wilcox4357@gmail.com>
2023-04-19 18:48:13 -0700

    wk 7 session 1 & 2

--- week6/day3/IADBMockExam2/client/src/App.js
+++ week6/day3/IADBMockExam2/client/src/App.js
@@ -1,21 +1,40 @@
 import './App.css';
+import { useState } from 'react';
 import Display from './components/Display';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Nav from './components/Nav';
 import CreateAlbum from './components/CreateAlbum';
 import OneAlbum from './components/OneAlbum';
 import EditAlbum from './components/EditAlbum';
 
 function App() {
+  const [album, setAlbum] = useState({
+    albumName: '',
+    artist: '',
+    releaseYear: 1920,
+    genre: 'Rock',
+    explicit: false
+  })
+  const [errors, setErrors] = useState({})
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav/>
+        <Nav />
         <Routes>
-          <Route path='/' element={<Display/>}/>
-          <Route path='/createAlbum/form' element={<CreateAlbum/>}/>
-          <Route path='/oneAlbum/:id' element={<OneAlbum/>}/>
-          <Route path='/editAlbum/:id' element={<EditAlbum/>}/>
+          <Route path='/' element={<Display />} />
+          <Route path='/createAlbum/form' element={<CreateAlbum 
+            album={album} 
+            setAlbum={setAlbum} 
+            errors={errors} 
+            setErrors={setErrors}/>} 
+          />
+          <Route path='/oneAlbum/:id' element={<OneAlbum />} />
+          <Route path='/editAlbum/:id' element={<EditAlbum 
+            album={album} 
+            setAlbum={setAlbum} 
+            errors={errors} 
+            setErrors={setErrors}
+          />} />
         </Routes>
       </BrowserRouter>
     </div>
